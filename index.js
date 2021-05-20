9385552c741ba738b21c9845c8b4691304ab0b60
Blunem <omark_009@hotmail.com>
2021-05-20 14:39:47 -0600

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('body', function getBody (req) {
     let body = 'No data'
     if (req.method === 'POST' )
