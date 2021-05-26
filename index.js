b5f2c9e9ac8d56dd6d123bb098293f005c13c93b
Blunem <omark_009@hotmail.com>
2021-05-26 17:11:08 -0600

    uibuild

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const app = express()
+
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
-
+const uniqueValidator = require('mongoose-unique-validator')
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
+app.disable('x-powered-by')
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
+  console.error(error.name)
+  if (error.name === 'CastError') 
     return response.status(400).send({ error: 'malformatted id' })
-  }
+  else if (error.name === 'ValidationError') 
+    return response.status(400).json({ error: error.message })
   next(error)
 }
 
@@ -36,22 +38,22 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log(body)
-      if (!body.name)
-          return response.status(400).json({error: 'name missing'}) 
-      
-      if (!body.number)
-          return response.status(400).json({error: 'number missing'}) 
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      response.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
+
   
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      })
-      person.save().then(savedNote => {
-        response.json(savedNote)
-      })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -72,20 +74,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  
+  Person.findByIdAndUpdate(request.params.id, {number : body.number}, {new:true ,runValidators: true })
     .then(updatedPerson => {response.json(updatedPerson)})
     .catch(error => next(error))
 })
+
 
 app.get('/info', (request, response) => {
     Person.find({}).then(person => {
       response.send(`<div>the phonebook has info from ${person.length} people </div><hr> <div>${new Date ()}</div>`)
     })
   })
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -94,7 +95,6 @@ app.get('/', (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
 app.use(errorHandler)
 const PORT = process.env.PORT
--- models/person.js
+++ models/person.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
+console.log(uniqueValidator)
 const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
@@ -13,9 +13,19 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true
+  }
 })
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -24,5 +34,6 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
 
 module.exports = mongoose.model('Person', personSchema)
